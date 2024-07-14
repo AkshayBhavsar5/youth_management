c9ec28039ca387c65fcf9129806672b4b08b68a5
sajju234 <sajudarji17@gmail.com>
2024-07-14 15:23:55 +0530

    Data Feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import DataFeed from "./components/ui/DataFeed";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className='px-2'>
-      <button>click me</button>
-    </div>
+      <div className="px-2">
+        <DataFeed />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
