64bb86a5468ae762f4dee2b2371e05613de7c18b
sajju234 <sajudarji17@gmail.com>
2024-07-14 18:44:23 +0530

    Fiter input

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
-
+import Filtes from "./components/Dashboard/Filters/Filtes";
 function Layout() {
   return (
     <>
       <Header />
       <Outlet />
+      <Filtes />
       <Dashboard />
     </>
   );
