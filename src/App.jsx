b3859fb0e110cbf0dbac8c7dab353e977bd5c9c4
Rakesh <rakeshm1714@gmail.com>
2024-07-14 16:58:37 +0530

    er

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-<<<<<<< HEAD
-
-import DataFeed from "./components/ui/DataFeed";
-import Header from "./components/Header/Header";
-// <<<<<<< HEAD
-import Login from "./components/Login/Login";
-// =======
-import Login from "./components/Header/Login/Login";
-// >>>>>>> 5097b31e411111d4a50d34fd7674aecc10712b37
-=======
 import DataFeed from "./components/DataFeed/DataFeed";
 import Header from "./components/Header/Header";
->>>>>>> ba29ef1da606cdb38f549e9ea8014d0a49cb195c
 import Register from "./components/Register/Register";
 
 // import Chat from "./components/Chat/Chat"
