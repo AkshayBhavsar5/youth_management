5097b31e411111d4a50d34fd7674aecc10712b37
sajju234 <sajudarji17@gmail.com>
2024-07-14 16:35:07 +0530

    sajju new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import DataFeed from "./components/ui/DataFeed";
+import Header from "./components/Header/Header";
+import Login from "./components/Header/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +14,9 @@ function App() {
     <>
       <div className="px-2">
         <button>click me</button>
+        <Header />
+        <Login />
+        <Register />
         <DataFeed />
       </div>
     </>
--- src/components/ui/DataFeed.jsx
+++ src/components/ui/DataFeed.jsx
@@ -1,37 +1,57 @@
 import React, { useState } from "react";
 import "tailwindcss/tailwind.css";
+import Select from "react-select"; // Import react-select
 
-const karyakarta = ["Jigar Patel", "Chetn Pipaliya", "Vivek Makvana"];
-const cities = ["home", "outside"];
+const karyakarta = [
+  { value: "Jigar Patel", label: "Jigar Patel" },
+  { value: "Chetn Pipaliya", label: "Chetn Pipaliya" },
+  { value: "Vivek Makvana", label: "Vivek Makvana" },
+];
+
+const cities = ["Home", "Outside"];
+
 const youthType = ["Student", "Businessman", "Employee"];
-const yuvaName = ["Sahjanand darji", "Akshay bhavsar", "Rkesh makwana"];
+
+const yuvaName = [
+  { value: "Sahjanand darji", label: "Sahjanand darji" },
+  { value: "Akshay bhavsar", label: "Akshay bhavsar" },
+  { value: "Rkesh makwana", label: "Rkesh makwana" },
+];
 
 const DataFeed = () => {
   const [formData, setFormData] = useState({
     date: "",
-    name: "",
+    karyakartaName: "",
+    yuvaName: "",
     place: "",
-    time: "",
+    hours: 0,
+    minutes: 0,
     category: "",
     comments: "",
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (name, selectedOption) => {
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: selectedOption.value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const { hours, minutes } = formData;
+    const totalMinutes = parseInt(hours) * 60 + parseInt(minutes);
+    if (totalMinutes > 120) {
+      alert("The total duration cannot exceed 2 hours.");
+      return;
+    }
+
     // Perform form validation and submission logic here
     console.log("Form submitted:", formData);
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100  py-5">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-5">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
         <h1 className="text-2xl font-bold mb-4 text-center">
           Data Feeder Form
@@ -44,7 +64,9 @@ const DataFeed = () => {
                 type="date"
                 name="date"
                 value={formData.date}
-                onChange={handleChange}
+                onChange={(e) =>
+                  setFormData({ ...formData, date: e.target.value })
+                }
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
@@ -53,43 +75,33 @@ const DataFeed = () => {
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700">
               Karyakrta Name:
-              <select
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
+              <Select
+                options={karyakarta}
+                value={karyakarta.filter(
+                  (option) => option.value === formData.karyakartaName
+                )}
+                onChange={(selectedOption) =>
+                  handleChange("karyakartaName", selectedOption)
+                }
+                placeholder="Select a name"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              >
-                <option value="" disabled>
-                  Select a name
-                </option>
-                {karyakarta.map((KK, index) => (
-                  <option value={KK} key={index}>
-                    {KK}
-                  </option>
-                ))}
-              </select>
+              />
             </label>
           </div>
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700">
               Yuva Name:
-              <select
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
+              <Select
+                options={yuvaName}
+                value={yuvaName.filter(
+                  (option) => option.value === formData.yuvaName
+                )}
+                onChange={(selectedOption) =>
+                  handleChange("yuvaName", selectedOption)
+                }
+                placeholder="Select a name"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              >
-                <option value="" disabled>
-                  Select a name
-                </option>
-                {yuvaName.map((Yn, index) => (
-                  <option value={Yn} key={index}>
-                    {Yn}
-                  </option>
-                ))}
-              </select>
+              />
             </label>
           </div>
           <div className="mb-4">
@@ -98,7 +110,9 @@ const DataFeed = () => {
               <select
                 name="place"
                 value={formData.place}
-                onChange={handleChange}
+                onChange={(e) =>
+                  setFormData({ ...formData, place: e.target.value })
+                }
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               >
@@ -116,14 +130,35 @@ const DataFeed = () => {
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700">
               Time:
-              <input
-                type="time"
-                name="time"
-                value={formData.time}
-                onChange={handleChange}
-                required
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
+              <div className="flex space-x-2">
+                <input
+                  type="number"
+                  name="hours"
+                  value={formData.hours}
+                  onChange={(e) =>
+                    setFormData({ ...formData, hours: e.target.value })
+                  }
+                  min="0"
+                  max="2"
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Hours"
+                />
+                <input
+                  type="number"
+                  name="minutes"
+                  value={formData.minutes}
+                  onChange={(e) =>
+                    setFormData({ ...formData, minutes: e.target.value })
+                  }
+                  min="0"
+                  max="60"
+                  step="15"
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Minutes"
+                />
+              </div>
             </label>
           </div>
           <div className="mb-4">
@@ -132,7 +167,9 @@ const DataFeed = () => {
               <select
                 name="category"
                 value={formData.category}
-                onChange={handleChange}
+                onChange={(e) =>
+                  setFormData({ ...formData, category: e.target.value })
+                }
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               >
@@ -153,7 +190,9 @@ const DataFeed = () => {
               <textarea
                 name="comments"
                 value={formData.comments}
-                onChange={handleChange}
+                onChange={(e) =>
+                  setFormData({ ...formData, comments: e.target.value })
+                }
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 placeholder="Enter comments (optional)"
               ></textarea>
