9f7b09cd333631a5569929d7e813f068777c71b6
Rakesh <rakeshm1714@gmail.com>
2024-07-14 16:56:38 +0530

    apper

--- src/App.jsx
+++ src/App.jsx
@@ -23,27 +23,11 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className="px-2">
-        <button>click me</button>
-        {/* <<<<<<< HEAD */}
-        <Header />
-        <Login />
-        <Register />
-        =======
-        <Header />
-        <Login />
-        <Register />
-        {/* >>>>>>> 5097b31e411111d4a50d34fd7674aecc10712b37 */}
-        <DataFeed />
-      </div>
-=======
       {/* <div className="">
         <Header/>
         <Register/>
         {/* <Chat/> */}
       {/* </div> */}
->>>>>>> ba29ef1da606cdb38f549e9ea8014d0a49cb195c
     </>
   );
 }
